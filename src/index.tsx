161e929fc29a077dfc3852c1bd91a866f518405b
samuelochaba <109516201+samuelochaba@users.noreply.github.com>
2022-10-29 01:25:01 +0100

    add static analysis and pre commit hooks

--- src/index.tsx
+++ src/index.tsx
@@ -1,7 +1,4 @@
-import React from "react";
-import ReactDOM from "react-dom";
+import React from 'react';
+import ReactDOM from 'react-dom';
 
-ReactDOM.render(
-  <div> This is a dope react app</div>,
-  document.getElementById("root")
-);
+ReactDOM.render(<div> This is a dope react app</div>, document.getElementById('root'));
