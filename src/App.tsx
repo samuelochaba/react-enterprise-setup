cdffbcaf668c1e7448d0e741b935a2fe90347bf9
samuelochaba <109516201+samuelochaba@users.noreply.github.com>
2022-11-03 16:13:01 +0100

    feat: add a simple form with TDD

--- src/App.tsx
+++ src/App.tsx
@@ -1,3 +1,33 @@
-import React from 'react';
+import { Button, Input } from '@mui/material';
+import React, { useState } from 'react';
 
-export const App = () => <div>This is a awesome fixed not so new react app</div>;
+export const App = () => {
+    const [itemDescription, setItemDescription] = useState<string>('');
+    const [items, setItems] = useState<string[]>([]);
+    return (
+        <div>
+            <Input
+                value={itemDescription}
+                onChange={(e) => {
+                    e.preventDefault();
+                    setItemDescription(e.target.value);
+                }}
+            />
+            <Button
+                onClick={() => {
+                    if (itemDescription !== '') {
+                        setItems([...items, itemDescription]);
+                        setItemDescription('');
+                    }
+                }}
+            >
+                Create
+            </Button>
+            <ul>
+                {items.map((item, i) => (
+                    <li key={i}>{item}</li>
+                ))}
+            </ul>
+        </div>
+    );
+};
